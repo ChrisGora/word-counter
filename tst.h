be4c29f0cf0ba1dbfaabb6b4a1c8d3752b0546f9
Chris Gora <kg17815@my.bristol.ac.uk>
2017-11-23 20:08:55 +0000

    Started extra work on TST - tst.h

---
+++ tst.h
@@ -0,0 +1,22 @@
+/* This is a library module supporting Ternary Search Trees (TSTs).
+It allows the user to store strings and associated values.
+The characters are stored in nodes.
+Each node has 3 children: smaller (left), equal (middle) and larger (right).
+This avoids using a large number of NULL pointers as with an R-way Trie
+*/
+
+struct tst;
+typedef struct tst tst;
+
+// Create a new empty TST and return a pointer to it.
+tst *newTst();
+
+// Insert a string into a TST. It is illegal to inser a string that already exists.
+void insert(tst *t, int x, char *c);
+
+// Search for a string and return its associated value if found, or NULL if not found.
+int search(tst *t, char *c);
+
+// Remove a string by setting its value to NULL. If possible also remove any 
+// unused nodes. 
+int remove(tst *t, char *c);
